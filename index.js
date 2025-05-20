94ebfa42f74f726241438af125c4ad4961c7c978
ritik143sg <ritik143sg@gmail.com>
2025-05-20 17:06:38 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const mainHeading = document.getElementById("main-heading");
+const header = document.getElementById("header");
+const thanks = document.getElementById("thanks");
+const basketHeading = document.getElementById("basket-heading");
+
+mainHeading.innerHTML = "Fruit World";
+mainHeading.style.color = "orange";
+
+header.style.backgroundColor = "green";
+
+header.style.borderBottomColor = "orange";
+header.style.borderStyle = "solid";
+header.style.borderWidth = "3px";
+
+basketHeading.style.color = "green";
+
+thanks.innerHTML = "<p>Please visit us again</p>";
